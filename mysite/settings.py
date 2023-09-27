8dafee4f08eaa11c4caae685aba3934448c66028
amarin20 <amarin20@ivytech.edu>
2023-09-26 21:03:10 -0400

    Docker

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zu1mg!%pws0j&s^0+&^o&9ag7+dtybc953*=_8icq$)i&ah4i@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1' , '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1' , '.pythonanywhere.com', 'localhost']
 
 
 # Application definition
